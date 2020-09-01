4735e59706e46f0bd84065286537f14685426b5d
Rosh97 <67596904+Rosh97@users.noreply.github.com>
2020-09-01 10:28:07 -0400

    Update TwitterHttpHelper.java

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
@@ -31,9 +31,6 @@ public class TwitterHttpHelper implements HttpHelper {
         consumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY,
                 CONSUMER_SECRET);
         consumer.setTokenWithSecret(ACCESS_TOKEN, TOKEN_SECRET);
-        /**
-         * Default = single connection. Discuss source code if time permit
-         */
         httpClient = new DefaultHttpClient();
     }
 
@@ -42,19 +39,7 @@ public class TwitterHttpHelper implements HttpHelper {
         consumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
        consumer.setTokenWithSecret(accessToken, tokenSecret);
         httpClient = HttpClientBuilder.create().build();
-        /**
-         * Default = single connection.
-         */
     }
-
-    //public TwitterHttpHelper(){
-        //String CONSUMER_KEY = System.getenv("consumerKey");
-        //String CONSUMER_SECRET = System.getenv("consumerSecret");
-        //String ACCESS_TOKEN = System.getenv("accessToken");
-        //String TOKEN_SECRET = System.getenv("tokenSecret");
-        //consumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-        //consumer.setTokenWithSecret(ACCESS_TOKEN, TOKEN_SECRET);
-    //}
 
     @Override
     public HttpResponse httpPost(URI uri) {
